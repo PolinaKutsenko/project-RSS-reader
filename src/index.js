8d65d4c873dd97b6c385c3130671e2dc1fee3e8d
Polina Kutsenko <polly153@mail.ru>
2022-10-05 14:51:25 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import './style.scss';
+import * as bootstrap from 'bootstrap';
+
+const app = () => {
+  const container = document.querySelector('.container');
+  const form = document.createElement('form');
+  form.innerHTML = '<input type="text" name="text"><input type="submit" value="Submit" disabled="">';
+  container.append(form);
+};
+app();
