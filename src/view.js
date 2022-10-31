100a8d6c6b2b07c713cf2e8ddf86260072252f6a
Polina Kutsenko <polly153@mail.ru>
2022-10-31 20:51:09 +0300

    update code

--- src/view.js
+++ src/view.js
@@ -7,29 +7,52 @@ import timer from './handlers/updateHandler.js';
 
 const watchedState = (state) => {
   const watcher = onChange(state, (path) => {
-    switch (path) {
-      case ('validation'):
-        formRender(watcher);
-        break;
-      case ('feedbackMessage'):
-        feedbackMessageRender(watcher);
-        break;
-      case ('loadingRSS.feeds'):
-      case ('loadingRSS.posts'):
-      case ('loadingRSS.uiState.viewedPostsId'):
-        renderResponse(watcher);
-        openPostLinkHandler(watcher);
-        viewModalOpenHandler(watcher);
-        break;
-      case ('loadingRSS.uiState.currentModal'):
-        buildModal(watcher);
-        break;
-      case ('loadingRSS.updatingPosts.currentTimerID'):
-        timer(watcher);
-        break;
-      default:
-        throw new Error('error in watcher');
+    if (path === 'validation') {
+      formRender(watcher);
     }
+    if (path === 'feedbackMessage') {
+      feedbackMessageRender(watcher);
+    }
+    if (path === 'loadingRSS.feeds') {
+      renderResponse(watcher);
+      openPostLinkHandler(watcher);
+      viewModalOpenHandler(watcher);
+    }
+    if (path === 'loadingRSS.posts' || path === 'loadingRSS.uiState.viewedPostsId') {
+      renderResponse(watcher);
+      openPostLinkHandler(watcher);
+      viewModalOpenHandler(watcher);
+    }
+    if (path === 'loadingRSS.uiState.currentModal') {
+      buildModal(watcher);
+    }
+    if (path === 'loadingRSS.updatingPosts.currentTimerID') {
+      timer(watcher);
+    }
+
+    //    switch (path) {
+    //      case ('validation'):
+    //        formRender(watcher);
+    //        break;
+    //      case ('feedbackMessage'):
+    //       feedbackMessageRender(watcher);
+    //        break;
+    //      case ('loadingRSS.feeds'):
+    //     case ('loadingRSS.posts'):
+    //     case ('loadingRSS.uiState.viewedPostsId'):
+    //       renderResponse(watcher);
+    //       openPostLinkHandler(watcher);
+    //       viewModalOpenHandler(watcher);
+    //       break;
+    //     case ('loadingRSS.uiState.currentModal'):
+    //       buildModal(watcher);
+    //       break;
+  //    case ('loadingRSS.updatingPosts.currentTimerID'):
+  //      timer(watcher);
+  //      break;
+  //    default:
+  //      throw new Error('error in watcher');
+    //   }
   });
   return watcher;
 };
